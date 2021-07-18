18da4e0ae840ea8ee23d59a80f186f8773d27835
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2021-07-18 11:23:24 +0530

    feat: working styled components model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import { nanoid } from "nanoid";
+import { compile, serialize, stringify } from "stylis";
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const classNameCache = new Map();
+
+const createAndInjectCSSClassName = (className, styles) => {
+  const styleSheet = document.styleSheets[0];
+  styleSheet.insertRule(
+    `
+      .${className} {
+        ${styles}
+      }
+    `,
+  );
+};
+
+const getExistingClassNameIfExists = (styles) => classNameCache.has(styles);
+
+const preprocessStyles = (styles) => serialize(compile(styles), stringify);
+
+const generateUniqueClassname = (styles) => {
+  const uniqueId = nanoid(10);
+  classNameCache.set(styles, uniqueId);
+  return uniqueId;
+};
+
+const stylish = (Tag) => (styles) => {
+  return function NewComponent({ children, ...props }) {
+    const preprocessedStyles = preprocessStyles(styles);
+
+    let className = getExistingClassNameIfExists(preprocessStyles);
+
+    if (!className) {
+      className = generateUniqueClassname(preprocessedStyles);
+
+      createAndInjectCSSClassName(className, preprocessedStyles);
+    }
+
+    const combinedClassNames = props.className
+      ? [className, props.className].join(" ")
+      : className;
+
+    return (
+      <Tag className={combinedClassNames} {...props}>
+        {children}
+      </Tag>
+    );
+  };
+};
+
+export default stylish;
